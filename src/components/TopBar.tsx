2895982113605a1fb929a52dc5bf5281c448e3bb
dosartcz <dosartcz@gmail.com>
2026-05-12 17:52:59 +0200

    Remove sticky header

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export function TopBar() {
   return (
-    <header className="sticky top-0 z-30">
+    <header>
       <div className="mx-auto flex w-full max-w-5xl items-center justify-end gap-3 px-4 py-3 sm:px-6 lg:px-8">
         <ThemeToggle />
         <LanguageSwitch />
